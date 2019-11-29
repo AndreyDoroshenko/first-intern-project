f87e87969d2a1423bc51e3aff7b490be6d8efd06
AndreyDoroshenko <doroshenko.exceedteam@gmail.com>
2019-11-29 14:17:21 +0300

    linked with data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import { messages } from './messages';
+import {messages} from './messages';
 // Подключение css
 import './css/styles.css';
 
-const container = document.querySelector('.container');
-container.classList.add('container');
+const todoList = [
+    {id: 1, text: 'Learn JS', isDone: false},
+    {id: 2, text: 'Learn React', isDone: true},
+    {id: 3, text: 'Make frontend great again', isDone: false},
+];
 
-const h1 = document.createElement('h1');
-h1.innerText = messages.h1;
 
-container.appendChild(h1);
+
+function createLine( id, lineText, isDone ){
+    let div = document.createElement('div');
+    div.id = 'list-'+ id;
+    let input = document.createElement('input');
+    let text = document.createElement('div');
+    let button = document.createElement('button');
+    div.classList.add('list__list-line');
+    input.type = 'checkbox';
+    input.checked = isDone;
+    text.classList.add('list__list-line-text');
+    button.classList.add('list__line-delete-button');
+    text.innerText = lineText;
+    button.innerText = 'X';
+
+    div.appendChild(input);
+    div.appendChild(text);
+    div.appendChild(button);
+    return div;
+}
+let list = document.querySelector('.list__list');
+/*todoList.forEach((line) => {
+    const elem = createLine(line.id,line.text, line.isDone);
+    list.appendChild(elem);
+});*/
+
+//let list = document.querySelector('.list__list');
+
+for (let i=0;i<todoList.length; i++ ){
+    let elem= createLine(todoList[i].id,todoList[i].text,todoList[i].isDone);
+    list.appendChild(elem);
+}
