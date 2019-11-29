31472fbce1854da07342ef622b98af0a1715e5de
AndreyDoroshenko <doroshenko.exceedteam@gmail.com>
2019-11-29 15:47:39 +0300

    data creation added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const todoList = [
     {id: 3, text: 'Make frontend great again', isDone: false},
 ];
 
-
-
 function createLine( id, lineText, isDone ){
     let div = document.createElement('div');
     div.id = 'list-'+ id;
@@ -18,7 +16,7 @@ function createLine( id, lineText, isDone ){
     let button = document.createElement('button');
     div.classList.add('list__list-line');
     input.type = 'checkbox';
-    input.checked = isDone;
+    input.checked = isDone || false;
     text.classList.add('list__list-line-text');
     button.classList.add('list__line-delete-button');
     text.innerText = lineText;
@@ -27,17 +25,26 @@ function createLine( id, lineText, isDone ){
     div.appendChild(input);
     div.appendChild(text);
     div.appendChild(button);
+
     return div;
 }
 let list = document.querySelector('.list__list');
-/*todoList.forEach((line) => {
+todoList.forEach((line) => {
     const elem = createLine(line.id,line.text, line.isDone);
     list.appendChild(elem);
-});*/
+});
 
-//let list = document.querySelector('.list__list');
-
-for (let i=0;i<todoList.length; i++ ){
-    let elem= createLine(todoList[i].id,todoList[i].text,todoList[i].isDone);
-    list.appendChild(elem);
+function addTodoItem(){
+    let newTodo=document.querySelector('.list__new-input');
+    if (newTodo.value.match(/\w+/)) {
+        todoList.push({id: (todoList.length + 1), text: newTodo.value, isDone: false});
+        const elem = createLine(todoList.length, newTodo.value, false);
+        list.appendChild(elem);
+    }
+    newTodo.value = '';
 }
+
+let addButton=document.querySelector('.list__new-button');
+addButton.addEventListener('click', addTodoItem);
+
+
