a2a167e49e8a365ff65a8ade327a7f8ac127e6d3
AndreyDoroshenko <doroshenko.exceedteam@gmail.com>
2019-12-02 12:09:35 +0300

    saving data

--- src/index.js
+++ src/index.js
@@ -2,63 +2,66 @@ import {messages} from './messages';
 // Подключение css
 import './css/styles.css';
 
-const todoList = [
-    {id: 1, text: 'Learn JS', isDone: false},
-    {id: 2, text: 'Learn React', isDone: true},
-    {id: 3, text: 'Make frontend great again', isDone: false},
-];
+const todoList = [];
 
-function createLine( id, lineText, isDone ){
+function createLine(id, lineText, isDone) {
     let div = document.createElement('div');
-    div.id = 'list-'+ id;
+    div.id = 'list-' + id;
     let input = document.createElement('input');
     let text = document.createElement('div');
     let button = document.createElement('button');
     div.classList.add('list__list-line');
     input.type = 'checkbox';
     input.checked = isDone || false;
-    input.addEventListener('change', switchState);
+    input.addEventListener('change', switchState(id));
     text.classList.add('list__list-line-text');
     button.classList.add('list__line-delete-button');
     text.innerText = lineText;
     button.innerText = 'X';
-
-    function switchState() {                                            // make with closure
-        let checkbox = parseInt(this.parentNode.id.slice(5));
-        todoList.forEach((line) => {
-            if (line.id == checkbox) {
-                line.isDone = !line.isDone;
-
-            }
-        })
-        console.log(todoList);
-    }
-
     div.appendChild(input);
     div.appendChild(text);
     div.appendChild(button);
-
     return div;
 }
 
-function addTodoItem(){
-    let newTodo=document.querySelector('.list__new-input');
+function updateData (record) {
+    const serialObj= JSON.stringify(record);
+    localStorage.setItem(record.id, serialObj);
+}
+
+function getData () {
+    for (let i=1; (JSON.parse((localStorage.getItem(i.toString())))); i++) {
+        todoList.push(JSON.parse((localStorage.getItem(i.toString()))));
+    }
+}
+
+const switchState = (id) => () => {
+        const edited = todoList.find((line) => line.id === id);
+        if (edited) {
+            edited.isDone = !edited.isDone;
+            updateData(edited);
+        }
+};
+
+function addTodoItem() {
+    let newTodo = document.querySelector('.list__new-input');
     if (newTodo.value.match(/\w+/)) {
         todoList.push({id: (todoList.length + 1), text: newTodo.value, isDone: false});
         const elem = createLine(todoList.length, newTodo.value, false);
-
+        updateData({id: todoList.length, text: newTodo.value , isDone: false});
         list.appendChild(elem);
+        console.log(todoList);
     }
     newTodo.value = '';
 }
 
-
-
-let addButton=document.querySelector('.list__new-button');
+let addButton = document.querySelector('.list__new-button');
 addButton.addEventListener('click', addTodoItem);
 
 let list = document.querySelector('.list__list');
+getData();
 todoList.forEach((line) => {
-    const elem = createLine(line.id,line.text, line.isDone);
+    const elem = createLine(line.id, line.text, line.isDone);
     list.appendChild(elem);
 });
+
