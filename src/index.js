09498c1b2d654bc5e34615cc6cffbadb5d4aa26a
AndreyDoroshenko <doroshenko.exceedteam@gmail.com>
2019-12-04 19:24:07 +0300

    to do list with style

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ function createLine(id, lineText, isDone) {
   const div = document.createElement('div');
   div.id = 'list-' + id;
   const input = document.createElement('input');
+  const label = document.createElement('label');
+  const snap = document.createElement('snap');
   const text = document.createElement('div');
-  const button = document.createElement('button');
+  //  const button = document.createElement('button');
   input.type = 'checkbox';
+  input.id = 'checkbox';
+  input.classList.add('list-line__checkbox__check-mark');
   input.checked = isDone || false;
+  snap.classList.add('list-line__checkbox__image');
   input.addEventListener('change', switchState(id));
-  button.addEventListener('click', deleteLine(id));
+  label.classList.add('list-line__checkbox');
+  //  button.addEventListener('click', deleteLine(id));
   text.classList.add('list-line__text');
-  button.classList.add('list-line__delete-button');
+  //  button.classList.add('list-line__delete-button');
   text.innerText = lineText;
-  button.innerText = 'X';
+  //  button.innerText = '\u274C';
   if (input.checked === true) {
     div.classList.add('list-line');
     div.classList.add('list-line_completed');
@@ -23,9 +29,11 @@ function createLine(id, lineText, isDone) {
     div.classList.add('list-line');
     div.classList.add('list-line_active');
   }
-  div.appendChild(input);
+  label.appendChild(input);
+  label.appendChild(snap);
   div.appendChild(text);
-  div.appendChild(button);
+  div.appendChild(label);
+  //  div.appendChild(button);
   return div;
 }
 
@@ -41,13 +49,13 @@ function getData() {
 }
 
 //  Удаление строки списка по нажатию кнопки
-const deleteLine = (id) => () => {
+/*  const deleteLine = (id) => () => {
   const div = document.getElementById('list-' + id);
   div.remove();
   const deleted = todoList.findIndex((line) => line.id === id);
   todoList.splice(deleted, 1);
   updateData();
-};
+}; */
 
 // Смена состояния строки списка
 const switchState = (id) => () => {
@@ -57,7 +65,7 @@ const switchState = (id) => () => {
     updateData();
   }
   const line = document.getElementById('list-' + id);
-  const input = line.firstChild;
+  const input = line.lastChild.firstChild;
   if (input.checked === true) {
     line.className = 'list-line ' + 'list-line_completed';
   } else {
@@ -67,18 +75,20 @@ const switchState = (id) => () => {
 
 //  Добавление новой строки списка
 function addTodoItem() {
-  const newTodo = document.querySelector('.new__input');
-  if (newTodo.value.match(/\w+/)) {
-    todoList.push({ id: (todoList[0] ? todoList[todoList.length - 1].id + 1 : 1), text: newTodo.value, isDone: false });
-    const elem = createLine(todoList.length, newTodo.value, false);
+  const newTodo = window.prompt('Enter new task', 'New task');
+  console.log(newTodo);
+  if (newTodo !== null && newTodo !== 'New task') {
+    todoList.push({ id: (todoList[0] ? todoList[todoList.length - 1].id + 1 : 1), text: newTodo, isDone: false });
+    const elem = createLine(todoList.length, newTodo, false);
     updateData(false);
     list.appendChild(elem);
   }
-  newTodo.value = '';
+  //  newTodo.value = '';
 }
 
 //  Установка фильтра на новое значение
 function setFilterTo(e) {
+  console.log(e.target);
   const activeFilter = e.target.name;
   updateListByFilter(activeFilter);
 }
@@ -88,17 +98,23 @@ function updateListByFilter(filter) {
   switch (filter) {
     case 'All': {
       const list = document.querySelector('.list');
+      const header = document.querySelector('h1');
+      header.innerHTML = filter + ' tasks';
       list.className = 'list';
       break;
     }
     case 'Completed': {
       const list = document.querySelector('.list');
+      const header = document.querySelector('h1');
+      header.innerHTML = filter + ' tasks';
       list.classList.add('list');
       list.classList.add('list_completed');
       break;
     }
     case 'Active': {
       const list = document.querySelector('.list');
+      const header = document.querySelector('h1');
+      header.innerHTML = filter + ' tasks';
       list.classList.add('list');
       list.classList.add('list_active');
       break;
@@ -115,8 +131,8 @@ function getUrlParameter(name) {
 };
 
 // Добавление обработчика для кнопки добавления строки в список
-const addButton = document.querySelector('.new__button');
-addButton.addEventListener('click', addTodoItem);
+const addNewTodoButton = document.querySelector('.add-new-button');
+addNewTodoButton.addEventListener('click', addTodoItem);
 
 //  Добавление обработчика для фильтра
 const filter = document.querySelector('.filter');
